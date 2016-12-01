39709155847229426b5d97646fde8aa39f152489
Brian Mock <brian@mockbrian.com>
2016-11-30 20:50:00 -0800

    classes() function update
    Needlessly complicates the classes() function to support objects (...for now)

--- js/classes.js
+++ js/classes.js
@@ -1,9 +1,21 @@
-function concat(a, b) {
-  return a.concat(b)
+function addClass(acc, x) {
+  let rest = ""
+  if (typeof x === "string") {
+    rest = x
+  } else if (Array.isArray(x)) {
+    rest = x.join(" ")
+  } else if (x && typeof x === "object") {
+    rest = Object.keys(x)
+      .filter(k => x[k])
+      .join(" ")
+  }
+  return (acc + " " + rest).trim()
 }
 
 function classes(...args) {
-  return args.reduce(concat, []).filter(Boolean).join(" ")
+  return args
+    .filter(Boolean)
+    .reduce(addClass, "")
 }
 
 module.exports = classes
