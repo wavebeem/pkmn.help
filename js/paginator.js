950f40bad8874efc9b2fab4220e56e35481c7c67
Brian Mock <brian@mockbrian.com>
2017-01-21 18:55:09 -0800

    Stop bouncing the page

--- js/paginator.js
+++ js/paginator.js
@@ -3,24 +3,27 @@ const classes = require("./classes")
 
 const $ = React.createElement
 
-const buttonClass = classes(
-  "db center",
-  "ba b--black-10 br3",
-  "bg-blue white",
-  "pv3 ph4",
-  "b f4",
-  "ttu",
-  "chunky-focus"
-)
-
-function prevButton({updatePagePrev}) {
-  const onClick = () => updatePagePrev()
-  return $("button", {onClick, className: buttonClass}, "Previous page")
+function buttonClass(show) {
+  return classes(
+    "db w-100",
+    "ba b--black-10 br3",
+    "bg-blue white",
+    "pv3 ph4",
+    "b f4",
+    "ttu",
+    "chunky-focus",
+    {hidden: !show}
+  )
 }
 
-function nextButton({updatePageNext}) {
+function prevButton({updatePagePrev}, show) {
+  const onClick = () => updatePagePrev()
+  return $("button", {onClick, className: buttonClass(show)}, "Previous page")
+}
+
+function nextButton({updatePageNext}, show) {
   const onClick = () => updatePageNext()
-  return $("button", {onClick, className: buttonClass}, "Next page")
+  return $("button", {onClick, className: buttonClass(show)}, "Next page")
 }
 
 function Paginator(props) {
@@ -38,9 +41,9 @@ function Paginator(props) {
   const i = pageSize * currentPage
   const pageItems = items.slice(i, i + pageSize)
   return $("div", {},
-    hasPrev ? prevButton(props) : null,
+    prevButton(props, hasPrev),
     $("div", {key: `page-${currentPage}`}, pageItems.map(render)),
-    hasNext ? nextButton(props) : null,
+    nextButton(props, hasNext),
   )
 }
 
