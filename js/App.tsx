66acc41cde86e016430b5b2bef14caa8b28e64ba
Brian Mock <brian@mockbrian.com>
2017-09-24 10:20:29 -0700

    fix a lil goof

--- js/App.tsx
+++ js/App.tsx
@@ -3,7 +3,7 @@ import * as Redux from "redux";
 import {connect} from "react-redux";
 
 import Offense from "./Offense";
-import Defense from "./defense";
+import Defense from "./Defense";
 import Dex from "./Dex";
 import {TabContainer, TabItem} from "./Tab";
 import {State} from "./store";
