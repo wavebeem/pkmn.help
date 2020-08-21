9d22efb445bcbaa3e52c105dbbb572487c224d6b
Brian Mock <brian@mockbrian.com>
2020-08-21 09:48:42 -0700

    big obj

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const fileLoader = {
-  test: /\.(png|svg)$/,
-  loader: "file-loader",
-  options: {
-    name: "files/[path][name].[contenthash:8].[ext]",
-  },
-  include: [path.resolve(__dirname, "img"), path.resolve(__dirname, "svg")],
-};
-
-const cssLoader = {
-  test: /\.css$/,
-  use: ["style-loader", "css-loader"],
-  // We need to load Tachyons which is in node_modules
-  // include: path.resolve(__dirname, "src")
-};
-
-const tsLoader = {
-  // Not actually loading any JS files any more, but whatever
-  test: /\.(ts|tsx|js)?$/,
-  loader: "ts-loader",
-  include: path.resolve(__dirname, "src"),
-};
-
 module.exports = {
   entry: [path.join(__dirname, "src/main.tsx")],
   output: {
@@ -40,7 +17,31 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   module: {
-    rules: [tsLoader, cssLoader, fileLoader],
+    rules: [
+      {
+        // Not actually loading any JS files any more, but whatever
+        test: /\.(ts|tsx|js)?$/,
+        loader: "ts-loader",
+        include: path.resolve(__dirname, "src"),
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        // We need to load Tachyons which is in node_modules
+        // include: path.resolve(__dirname, "src")
+      },
+      {
+        test: /\.(png|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "files/[path][name].[contenthash:8].[ext]",
+        },
+        include: [
+          path.resolve(__dirname, "img"),
+          path.resolve(__dirname, "svg"),
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
