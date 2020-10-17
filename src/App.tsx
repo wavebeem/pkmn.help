d07f6fe88561779af6073c8f9972c0c89af59944
Joshua Booth <joshua.n.booth@gmail.com>
2020-10-17 11:45:05 -0700

    Use URI hash for navigation
    Examples:
    pkmn.help/#offense/fighting+ground+fire+psychic
    pkmn.help/#defense/fairy+poison
    pkmn.help/#pokedex
    pkmn.help/#pokedex/3
    pkmn.help/#pokedex/saur
    pkmn.help/#pokedex/sa/3
    pkmn.help/#info

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Offense } from "./Offense";
 import { Defense } from "./Defense";
 import { InfoScreen } from "./InfoScreen";
 import { TabContainer, TabItem } from "./Tab";
-import { Type } from "./data";
+import { Type, types } from "./data";
 
 const Dex = React.lazy(async () => {
   const { Dex } = await import(
@@ -15,17 +15,89 @@ const Dex = React.lazy(async () => {
   return { default: Dex };
 });
 
+const hashFromTab = ["#offense", "#defense", "#pokedex", "#info"];
+const tabFromHash = new Map((Array.from(hashFromTab.entries(), kv => [kv[1], kv[0]])));
+const hashFromType = new Map(types.map(t => [t, t.valueOf()]));
+const typeFromHash = new Map(types.map(t => [t.valueOf(), t]));
+
+function typesFromHash(fragments?: string): Type[] {
+  if (!fragments) {
+    return [];
+  }
+  const types = [];
+  for (const fragment of fragments.split("+")) {
+    const type = typeFromHash.get(fragment);
+    if (type !== undefined) {
+      types.push(type);
+    }
+  }
+  return types;
+}
+
+const hashes = window.location.hash.split("/");
+let hashTab = tabFromHash.get(hashes[0]);
+if (hashTab === undefined) {
+  hashTab = 1;
+}
+let hashOffense: Type[] = [];
+let hashDefense: Type[] = [];
+let hashSearch = "";
+let hashPage = 1;
+switch (hashTab) {
+  case 0:
+    hashOffense = typesFromHash(hashes[1]);
+    break;
+  case 1:
+    hashDefense = typesFromHash(hashes[1]);
+    break;
+  case 2:
+    hashPage = parseInt(hashes[1]);
+    if (isNaN(hashPage)) {
+      hashSearch = hashes[1] || "";
+      hashPage = parseInt(hashes[2]) || 1;
+    }
+}
+let resetCurrentPage = false;
 export function App() {
-  const [tab, changeTab] = React.useState(1);
-  const [offenseTypes, updateOffenseTypes] = React.useState([] as Type[]);
-  const [type1, updateType1] = React.useState(Type.NORMAL);
-  const [type2, updateType2] = React.useState(Type.NONE);
-  const [currentPage, updateCurrentPage] = React.useState(0);
-  const [search, updateSearch] = React.useState("");
+  const [tab, changeTab] = React.useState(hashTab!);
+  const [offenseTypes, updateOffenseTypes] = React.useState(hashOffense);
+  const [type1, updateType1] = React.useState(hashDefense[0] || Type.NORMAL);
+  const [type2, updateType2] = React.useState(hashDefense[1] || Type.NONE);
+  const [currentPage, updateCurrentPage] = React.useState(hashPage - 1);
+  const [search, updateSearch] = React.useState(hashSearch);
 
   React.useEffect(() => {
-    updateCurrentPage(0);
+    if (resetCurrentPage) {
+      updateCurrentPage(0);
+    } else {
+      resetCurrentPage = true;
+    }
   }, [search]);
+
+  React.useEffect(() => {
+      let hash = hashFromTab[tab];
+      switch (tab) {
+        case 0:
+          hash += "/" + offenseTypes.map(x => hashFromType.get(x)).join("+");
+          break;
+
+        case 1:
+          hash += "/" + hashFromType.get(type1);
+          if (type2 !== Type.NONE) {
+            hash += "+" + hashFromType.get(type2);
+          }
+          break;
+
+        case 2:
+          if (search !== "") {
+            hash += "/" + search;
+          }
+          if (currentPage > 0) {
+            hash += "/" + (currentPage + 1);
+          }
+      }
+      history.replaceState(undefined, "Pokémon Type Calculator", hash);
+  }, [tab, offenseTypes, type1, type2, currentPage]);
 
   return (
     <div className="sans-serif bg-near-white near-black min-vh-100 flex flex-column">
