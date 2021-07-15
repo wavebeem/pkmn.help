cf907ed2b74aa78a958bd666da23b65a0189aee5
nguoianphu <nguoianphu@gmail.com>
2021-07-15 18:06:00 +0700

    Add github action to build Android apk by capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'help.pkmn',
+  appName: 'pkmn',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
