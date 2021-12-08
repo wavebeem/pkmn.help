93fec9da39890e3b7d57d089108b1f59ce1f50e0
Brian Mock <brian@wavebeem.com>
2021-12-07 17:22:51 -0800

    hmm

--- src/useUpdateSW.ts
+++ src/useUpdateSW.ts
@@ -2,26 +2,35 @@ import * as React from "react";
 
 interface UpdateSW {
   type: "updating" | "pending";
+  eventType: string;
 }
 
 export function useUpdateSW(): UpdateSW {
   // TODO: Don't check too frequently
-  const [state, setState] = React.useState<UpdateSW>({ type: "pending" });
+  const [state, setState] = React.useState<UpdateSW>({
+    type: "pending",
+    eventType: "",
+  });
   React.useEffect(() => {
-    const update = async () => {
+    const update = async (event: Event) => {
+      if (document.hidden) {
+        return;
+      }
       const reg = await navigator.serviceWorker.getRegistration();
       if (reg) {
         try {
-          setState({ type: "updating" });
+          setState({ type: "updating", eventType: event.type });
           await reg.update();
         } finally {
-          setState({ type: "pending" });
+          setState({ type: "pending", eventType: event.type });
         }
       }
     };
-    addEventListener("pageshow", update);
+    document.addEventListener("visibilitychange", update);
+    document.addEventListener("focus", update);
     return () => {
-      removeEventListener("pageshow", update);
+      document.removeEventListener("visibilitychange", update);
+      document.removeEventListener("focus", update);
     };
   }, []);
   return state;
