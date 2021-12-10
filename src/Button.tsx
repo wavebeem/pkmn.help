fad9da07ace37767538688d8413b4f1273ac7ddc
Brian Mock <brian@wavebeem.com>
2021-12-09 21:05:03 -0800

    Language picker (#123)

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,24 +4,14 @@ import * as React from "react";
 export function Button({
   className,
   ...props
-}: React.ButtonHTMLAttributes<never>) {
-  return (
-    <button
-      {...props}
-      tabIndex={0}
-      onClick={props.onClick}
-      className={classNames(
-        className,
-        baseClasses,
-        "border1 button-shadow button-bg button-bg-hover color-inherit active-squish"
-      )}
-    />
-  );
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return <button {...props} className={classNames(className, baseClasses)} />;
 }
 
 const baseClasses = classNames(
   "no-underline",
   "ba br2 pv1 ph2",
   "b f5",
-  "SimpleFocus"
+  "SimpleFocus",
+  "border1 button-shadow button-bg button-bg-hover color-inherit active-squish"
 );
---
+++ src/useLanguage.ts
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { useLocalStorage } from "usehooks-ts";
+
+export function useLanguage(): [
+  string,
+  React.Dispatch<React.SetStateAction<string>>
+] {
+  const [language, setLanguage] = useLocalStorage("language", "en");
+  return [language, setLanguage];
+}
