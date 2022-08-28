eca3084c9e95e592280eed14cb6a60a86ef3e0e3
Brian Mock <brian@wavebeem.com>
2022-08-28 12:50:44 -0700

    Fixes #141 localized links

--- bin/saveJSON.ts
+++ bin/saveJSON.ts
@@ -1,10 +1,14 @@
 import path from "path";
 import fs from "fs";
 
-export function saveJSON(filename: string, data: any): void {
+export function saveJSON(
+  filename: string,
+  data: any,
+  { indent = 0 }: { indent?: number } = {}
+): void {
   fs.writeFileSync(
     path.resolve(__dirname, filename),
-    JSON.stringify(data, null, 2),
+    JSON.stringify(data, null, indent),
     "utf-8"
   );
 }
