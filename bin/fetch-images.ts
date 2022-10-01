c69f5d814f765fb28809336664a77a50dfe1f12e
Brian Mock <brian@wavebeem.com>
2022-10-01 09:11:21 -0700

    ok

--- bin/fetch-images.ts
+++ bin/fetch-images.ts
@@ -1,30 +1,20 @@
 import fetch from "node-fetch";
 import path from "path";
 import fs from "fs";
+import { readJSON } from "./readJSON";
+import { saveJSON } from "./saveJSON";
 
 const SRC = path.resolve(__dirname, "../data/pokemon.json");
-const IMG_DEST = path.resolve(__dirname, "../img");
-const DATA_DEST = path.resolve(__dirname, "../src/data-pkmn.json");
+const IMG_DEST = path.resolve(__dirname, "../public/img");
+const DATA_DEST = path.resolve(__dirname, "../public/data-pkmn.json");
 
 async function fetchBuffer(url: string): Promise<Buffer> {
   const resp = await fetch(url);
   return await resp.buffer();
 }
 
-function loadJSON(filename: string): any {
-  return JSON.parse(fs.readFileSync(filename, "utf-8"));
-}
-
-function saveJSON(filename: string, data: any): void {
-  fs.writeFileSync(
-    path.resolve(__dirname, filename),
-    JSON.stringify(data, null, 2),
-    "utf-8"
-  );
-}
-
 async function main(): Promise<void> {
-  const list = loadJSON(SRC);
+  const list = readJSON(SRC);
   for (const item of list) {
     const imgFilename = path.resolve(IMG_DEST, `${item.id}.png`);
     if (item.spriteURL && !fs.existsSync(imgFilename)) {
