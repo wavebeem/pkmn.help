e4bc11c1796dc87426d8adaeb2ddc8c360c69be3
Sage Fennel <sage@wavebeem.com>
2024-05-01 22:35:41 -0700

    German update (#280)

--- src/ScreenMore.tsx
+++ src/ScreenMore.tsx
@@ -8,7 +8,6 @@ import { TranslationCard } from "./TranslationCard";
 import { compare } from "./compare";
 import { generations, isGeneration } from "./data-generations";
 import {
-  Lang,
   getDesiredLanguage,
   isLang,
   supportedLanguages,
@@ -18,106 +17,18 @@ import { useGeneration } from "./useGeneration";
 import { useLanguage } from "./useLanguage";
 import { useTheme } from "./useTheme";
 import { useTypeCount } from "./useTypeCount";
+import {
+  showLang,
+  formatLanguageCompletion,
+  officialLanguages,
+  unofficialLanguages,
+  languageBounty,
+  languageCompletions,
+} from "./lang";
 
-export interface ScreenMoreProps {
+interface ScreenMoreProps {
   needsAppUpdate: boolean;
   updateApp: () => Promise<void>;
-}
-
-const languageCompletions =
-  typeof __TRANSLATION_COMPLETION__ === "undefined"
-    ? {}
-    : __TRANSLATION_COMPLETION__;
-
-export const languageNamesNative: Record<Lang, string> = {
-  en: `English`,
-  es: `Español`,
-  "pt-PT": `Português (Portugal)`,
-  "pt-BR": `Português (Brasil)`,
-  de: `Deutsch`,
-  da: `Dansk`,
-  it: `Italiano`,
-  fr: `Français`,
-  ro: `Română`,
-  pl: `Polski`,
-  ru: `Русский`,
-  nl: `Nederlands`,
-  kk: `Қазақша`,
-  ja: `日本語`,
-  "ja-Hrkt": `にほんご`,
-  "zh-Hans": `简体中文`,
-  "zh-Hant": `繁體中文`,
-  ko: `한국어`,
-};
-
-const officialLanguages = [
-  "en",
-  "es",
-  "de",
-  "it",
-  "fr",
-  "ja",
-  "zh-Hans",
-  "zh-Hant",
-  "ko",
-] as const;
-
-export const officialLanguagesSet = new Set(officialLanguages);
-
-const unofficialLanguages = Object.keys(languageNamesNative).filter((lang) => {
-  return !officialLanguagesSet.has(lang as any);
-});
-
-export const languageNamesEnglish: Record<Lang, string> = {
-  en: ``,
-  es: `Spanish`,
-  "pt-PT": "Portuguese (Portugal)",
-  "pt-BR": `Portuguese (Brazil)`,
-  de: `German`,
-  da: `Danish`,
-  it: `Italian`,
-  fr: `French`,
-  ro: `Romanian`,
-  pl: `Polish`,
-  ru: `Russian`,
-  nl: `Dutch`,
-  kk: `Kazakh`,
-  ja: `Japanese`,
-  "ja-Hrkt": `Japanese Kana-only`,
-  "zh-Hans": `Simplified Chinese`,
-  "zh-Hant": `Traditional Chinese`,
-  ko: `Korean`,
-};
-
-export const languageBounty: Record<Lang, number> = {
-  en: 0,
-  es: 0,
-  "pt-PT": 0,
-  "pt-BR": 0,
-  de: 30,
-  da: 0,
-  it: 0,
-  fr: 0,
-  ro: 0,
-  pl: 0,
-  ru: 0,
-  nl: 0,
-  kk: 0,
-  ja: 50,
-  "ja-Hrkt": 0,
-  "zh-Hans": 0,
-  "zh-Hant": 0,
-  ko: 0,
-};
-
-export function formatLanguageCompletion(lang: string): string {
-  const value = languageCompletions[lang] || 0;
-  const n = Math.floor(value * 100);
-  return `${n}%`;
-}
-
-function showLang(lang: Lang): string {
-  return languageNamesNative[lang];
 }
 
 export function ScreenMore({
@@ -493,8 +404,6 @@ export function ScreenMore({
         <h3 className={classH3}>{t("more.thanks.sections.zh-Hans")}</h3>
         <ul className="list mb0 mt1 pl3">
           <li>Dragonify</li>
-        </ul>
-        <ul className="list mb0 mt1 pl3">
           <li>Tin</li>
         </ul>
 
@@ -522,6 +431,7 @@ export function ScreenMore({
 
         <h3 className={classH3}>{t("more.thanks.sections.de")}</h3>
         <ul className="list mb0 mt1 pl3">
+          <li>SpeciesSaladMallory</li>
           <li>Cozzzy</li>
           <li>Luzifer Senpai</li>
         </ul>
@@ -544,8 +454,6 @@ export function ScreenMore({
         <h3 className={classH3}>{t("more.thanks.sections.ko")}</h3>
         <ul className="list mb0 mt1 pl3">
           <li>BetterBritter</li>
-        </ul>
-        <ul className="list mb0 mt1 pl3">
           <li>Eric Marriott</li>
         </ul>
 
