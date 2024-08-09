90f2cc52dca58a1e85e0da87f86ea8ebcce8c8b1
Sage Fennel <sage@wavebeem.com>
2024-08-09 14:52:32 -0700

    add norwegian

--- src/misc/detectLanguage.ts
+++ src/misc/detectLanguage.ts
@@ -10,24 +10,37 @@ export function detectLanguage(): Lang | undefined {
 export function getDesiredLanguage(): Lang | undefined {
   for (const code of navigator.languages) {
     // If it's a language we support, use it as-is
-    if (isLang(code)) return code;
+    if (isLang(code)) {
+      return code;
+    }
     // Normalize country-centric language codes to the more generic
     // script-centric ones for Chinese languages
-    if (code === "zh-HK") return "zh-Hant";
-    if (code === "zh-TW") return "zh-Hant";
-    if (code === "zh-CN") return "zh-Hans";
-    if (code === "zh-SG") return "zh-Hans";
     // Otherwise just use the language code without the country code
-    const [lang] = code.split("-");
-    // Give preference to simplified Chinese for any other language code that
-    // wants a Chinese language... Not sure if that's really the best idea, but
-    // I have to pick something here...
-    if (code === "zh") return "zh-Hans";
+    const [lang, country] = code.split("-");
+    if (code === "nn" || code === "nb") {
+      return "no";
+    }
+    if (code === "zh") {
+      if (country === "HK" || country === "TW") {
+        return "zh-Hant";
+      }
+      if (country === "CN" || country === "SG") {
+        return "zh-Hans";
+      }
+      // Give preference to simplified Chinese for any other language code that
+      // wants a Chinese language... Not sure if that's really the best idea, but
+      // I have to pick something here...
+      return "zh-Hans";
+    }
     // Any Portuguese langauge besides Brazilian Portuguese should use European
     // Portuguese translation
-    if (code === "pt") return "pt-PT";
+    if (code === "pt") {
+      return "pt-PT";
+    }
     // If the language code minus the country code is a language we support
-    if (isLang(lang)) return lang;
+    if (isLang(lang)) {
+      return lang;
+    }
   }
   return undefined;
 }
@@ -49,6 +62,7 @@ export const supportedLanguages = [
   "fr",
   "pl",
   "ru",
+  "no",
   "nl",
   "kk",
   "ro",
