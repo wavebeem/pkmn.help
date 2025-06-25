4e68bbb95d3932235ee33186eb6ad54632553058
Mark Soh <marksoh115@gmail.com>
2025-06-25 15:00:28 -0700

    Add type icons for Pokémon types and update icon import logic

--- src/components/MultiTypeSelector.tsx
+++ src/components/MultiTypeSelector.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import { useTranslation } from "react-i18next";
+import { typeIcon } from "../misc/icons";
 import { typeColor, typeColorBG } from "../misc/colors";
 import { Generation } from "../misc/data-generations";
 import { Type, typesForGeneration } from "../misc/data-types";
@@ -37,10 +38,12 @@ export function MultiTypeSelector({
               isChecked && "focus-tab",
               !isChecked && "focus-simple",
             )}
+        
             style={customProperties({
               "--type-color-bg": typeColorBG(type),
               "--type-color": typeColor(type),
             })}
+
           >
             <Flex tag="span" gap="medium" justify="flex-start" align="center">
               <input
@@ -61,6 +64,12 @@ export function MultiTypeSelector({
                   }
                   onChange(newValue);
                 }}
+              />
+              <img 
+                src={typeIcon(type.charAt(0).toUpperCase() + type.slice(1))} 
+                alt={type} 
+                className="type-icon" 
+                style={{ width: '24px', height: '24px' }} 
               />
               {t(`types.${type}`)}
             </Flex>
