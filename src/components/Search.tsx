a69c28fccb80c28e70687bfe70efb1279842879f
Okapi1023 <lutfunjoya@gmail.com>
2025-10-15 18:11:35 +0100

    Refactor Search component to simplify structure

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,55 +1,20 @@
-import { clsx } from "clsx";
-import { ReactNode } from "react";
-import { customProperties } from "../misc/customProperties";
-import { Icon } from "./Icon";
+import React from "react";
 import styles from "./Search.module.css";
 
-interface SearchProps {
-  label: string;
-  helpText?: ReactNode;
+type Props = {
   value: string;
   onChange: (value: string) => void;
-}
+  placeholder?: string;
+};
 
-export function Search({
-  label,
-  helpText,
-  value,
-  onChange,
-}: SearchProps): ReactNode {
-  const iconSize = 24;
-  return (
-    <div
-      className={styles.root}
-      style={customProperties({
-        "--icon-size": `${iconSize}px`,
-      })}
-    >
-      {label && <div className={styles.label}>{label}</div>}
-      <div className={styles.wrapper}>
-        <Icon name="search" size={iconSize} className={styles.iconSearch} />
-        <input
-          type="text"
-          autoComplete="off"
-          autoCorrect="off"
-          inputMode="search"
-          autoCapitalize="none"
-          className={clsx(styles.input, "focus-simple")}
-          value={value}
-          onChange={(event) => {
-            onChange(event.target.value);
-          }}
-        />
-        <Icon
-          name="clear"
-          size={iconSize}
-          className={clsx(styles.iconClear, value === "" && styles.hidden)}
-          onClick={() => {
-            onChange("");
-          }}
-        />
-      </div>
-      {helpText && <p className={styles.help}>{helpText}</p>}
-    </div>
-  );
-}
+const Search: React.FC<Props> = ({ value, onChange, placeholder }) => (
+  <input
+    type="text"
+    className={styles.input}
+    value={value}
+    onChange={e => onChange(e.target.value)}
+    placeholder={placeholder || "Search Pokémon..."}
+  />
+);
+
+export default Search;
