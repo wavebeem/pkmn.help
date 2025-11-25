280311e08c2c350c0c9a7dad987d472da9beeff9
Sage Fennel <sage@wavebeem.com>
2025-11-25 14:59:00 -0800

    blah

--- src/hooks/useRouteChangeFixes.tsx
+++ src/hooks/useRouteChangeFixes.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export function useRouteChangeFixes() {
+export function useRouteChangeFixes(): void {
   const location = useLocation();
 
   useEffect(() => {
